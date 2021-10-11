381927b3cd8e97fad525f2b81c0da858048a86db
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2021-10-11 10:51:50 -0500

    Revert "Yeyo"

--- src/utils/sendgrid.util.ts
+++ src/utils/sendgrid.util.ts
@@ -1,19 +1,27 @@
-import sgMail from "@sendgrid/mail";
+import sgMail, { MailDataRequired } from '@sendgrid/mail';
 import dotenv from 'dotenv';
 dotenv.config();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
+if (process.env.SENDGRID_API_KEY) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+}
 
-const createEmail = (to: string, subject: string, message: string, link: string = '', token: string = '') => {
+const createEmail = (
+  to: string,
+  subject: string,
+  message: string,
+  link = '',
+  token = '',
+): MailDataRequired => {
   const msg: sgMail.MailDataRequired = {
     to,
     subject,
     from: 'joelvaldezangeles@gmail.com',
     html: `<h1>${subject}</h1>
     <p>${message}</p><p>${link}</p>
-    ${token ? `<p>or change the token in params<br>${token}</p>` : ''}`
-  }
-  return msg
-}
+    ${token ? `<p>or change the token in params<br>${token}</p>` : ''}`,
+  };
+  return msg;
+};
 
-export { sgMail, createEmail }
+export { sgMail, createEmail };
